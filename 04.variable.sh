e510492fba303d3b1d7f0729af8d2ee3c9d9117e
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-25 23:09:31 +0530

    Fourth

--- 04.variable.sh
+++ 04.variable.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 DATE=$(date)
+PWD=$(pwd)
+USER_ID=$(id -u)
+TIMEZONE_INFO=$(/usr/share/zoneinfo/Asia)
 
 echo "$DATE"
+echo "Present working directory: $PWD"
+echo "User information : $USER_ID"
+echo "All time zones in Asia: $TIMEZONE_INFO"
+
