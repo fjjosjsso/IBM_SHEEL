03c31a70e1ed936fac25271829701f6eec5b24b3
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-25 18:37:49 +0530

    Variable message

---
+++ 01.variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh: Hello Suresh! Good morning"
+echo "Suresh: Good morning Ramesh, How are you"
+echo "Ramesh: I am good what about you"
+echo "Suresh: I am also good thanks"
