900bc201e2270306d133fedb94dde7240489e816
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-25 18:54:27 +0530

    Secon

---
+++ 02.variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rohit
+PERSON2=Balu
+
+echo "$PERSON1: Hello $PERSON2! Good morning"
+echo "$PERSON2: Good morning $PERSON1, How are you"
+echo "$PERSON1: I am good what about you"
+echo "$PERSON2: I am also good thanks"
