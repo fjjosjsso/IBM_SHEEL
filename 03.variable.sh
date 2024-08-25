fe74647d3fc75e8f297b7a52d9d132f6273ebedb
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-25 22:59:12 +0530

    Second message

---
+++ 03.variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Good morning $PERSON2"
+echo "$PERSON2: Good morning, How are you"
+echo "$PERSON1: I am good what about you"
+echo "$PERSON2: I am also good thanks"
