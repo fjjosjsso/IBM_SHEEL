6c058bacb3d148bef1d35b826bf9e4eae3c07889
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-26 16:16:41 +0530

    function

---
+++ 01-function.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function check {
+    if [ $marks -gt 35 ]
+then
+    echo "YOUR ARE PASS"
+else
+    echo "YOUR ARE FAILD"
+fi
+}
+read -p "Enter your Telugu marks" marks
+check
+
+read -p "Enter your English marks" English_marks
+check
+
+read -p "Enter your maths marks" Maths_marks
+check
