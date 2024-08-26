d414479efb14f0835ca3b4ae4fb976dfaabc78f6
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-26 18:17:34 +0530

    Funcion

--- git.sh
+++ git.sh
@@ -4,7 +4,8 @@ USER_ID=$(id -u)
 if [ $USER_ID -ge 0 ]
 then
     echo "Your not in root access:: Kindly get root access with sudo su -"
+    
 else
     echo "Your are in root access"
-    yum install mysql -y
+    sudo yum install mysql -y
 fi
