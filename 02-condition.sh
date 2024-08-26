b59a090b4b3e26deffc4f20f89e74a502e7a05cc
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-26 15:27:19 +0530

    CONDITION

--- 02-condition.sh
+++ 02-condition.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 read -p "Enter your Telugu marks" marks
-if [ $marks -lt 35 ]
+if [ $marks -gt 35 ]
 then
-    echo "YOUR ARE FAILD"
-else
     echo "YOUR ARE PASS"
+else
+    echo "YOUR ARE FAILD"
 fi
 
 read -p "Enter your English marks" English_marks
-if [ $marks -lt 35 ]
+if [ $marks -gt 35 ]
 then
-    echo "YOUR ARE FAILD"
-else
     echo "YOUR ARE PASS"
+else
+    echo "YOUR ARE FAILD"
 fi
 
 read -p "Enter your maths marks" Maths_marks
-if [ $marks -lt 35 ]
+if [ $marks -gt 35 ]
 then
-    echo "YOUR ARE FAILD"
-else
     echo "YOUR ARE PASS"
+else
+    echo "YOUR ARE FAILD"
 fi
