15b34b2f07e4ebfca820694cb7b6658a1dc3f014
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-26 13:04:34 +0530

    second array

---
+++ 02-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+my_array_names=( sai ram sakaray sakare nani )
+
+echo "First array word: ${my_array_names[1]}"
+echo "Second array word : ${my_array_names[2]}"
+
+echo "last array word: ${my_array_names[4]}"
+
+echo "Print all names in array: ${my_array_names[*]}"
+echo "How many names are there: ${#my_array_names[*]}"
