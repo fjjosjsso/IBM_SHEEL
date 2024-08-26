61270018fee4a28acbbfa99812b2ccef94862456
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-26 17:38:51 +0530

    Funcion

---
+++ git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+if [ $USER_ID -ge 0 ]
+then
+    echo "Your not in root access:: Kindly get root access with sudo su -"
+else
+    echo "Your are in root access"
+    yum install git -y
+fi
