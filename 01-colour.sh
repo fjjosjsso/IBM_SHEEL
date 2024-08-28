4c47e501cf1d32faff090cfadf8c53bb4e55b289
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-28 16:41:55 +0530

    Red colour

--- 01-colour.sh
+++ 01-colour.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -e "\e[1;31m This is  colour \e[0m "
-echo -e "\e[2;32m This is  colour \e[0m "
-echo -e "\e[3;33m This is  colour \e[0m "
-echo -e "\e[4;34m This is  colour \e[0m "
-echo -e "\e[5;35m This is  colour \e[0m "
+echo -e "\e[1;31m This is  colour  "
+echo -e "\e[2;32m This is  colour  "
+echo -e "\e[3;33m This is  colour  "
+echo -e "\e[4;34m This is  colour  "
+echo -e "\e[5;35m This is  colour  "
