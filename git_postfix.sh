b0f2649b71b28e3b419455d7efc0ed8441432845
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-28 18:06:31 +0530

    Git and postfix

--- git_postfix.sh
+++ git_postfix.sh
@@ -11,12 +11,12 @@ else
 fi
 yum install git -y
 
-if [ $CHECK -ne 0 ]
+if [ $CHECK == 0 ]
 then
-    echo -e "\e[1;31m INSTALLING GIT FAILD \e[0m"
+    echo -e "\e[1;31m INSTALLING GIT SUCUSSES  \e[0m"
     exit 1
 else
-    echo -e "\e[1;32m INSTALLING GIT SUCUSSES \e[0m"
+    echo -e "\e[1;32m INSTALLING GIT faild \e[0m"
 fi
 
 if [ $USER_ID -ne 0 ]
@@ -26,12 +26,12 @@ then
 else
     echo -e "\e[1:32m INFO::: You are in root access \e[0m"
 fi
-yum install postfixx -y
-if [ $CHECK -ne 0 ]
+yum install postfixxxxx -y
+if [ $CHECK == 0 ]
 then
-    echo -e "\e[1;31m INSTALLING POSTFIX FAILD \e[0m"
+    echo -e "\e[1;31m INSTALLING POSTFIX SUCUSSES \e[0m"
     exit 1
 else
-    echo -e "\e[1;32m INSTALLING POSTFIX SUCUSSES \e[0m"
+    echo -e "\e[1;32m INSTALLING POSTFIX  fail \e[0m"
 fi
 
