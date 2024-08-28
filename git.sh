34733fee12892298125500fecb0a7fd1781d59c5
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-28 17:28:31 +0530

    git

--- git.sh
+++ git.sh
@@ -6,7 +6,7 @@ then
     echo -e "\e[1;31m Error:: Permission denied Kinly used with root access \e[1;0m"
         exit 1
 else
-    echo -e "e\1;32m INFO:: Your are in root access \e[1;0m"
+    echo -e "e\[1;32m INFO:: Your are in root access \e[1;0m"
     
 fi
 sudo yum install git -y
