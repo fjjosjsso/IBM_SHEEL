3124e415e21d3a5b9065be55a76b380f00e2de08
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-28 17:25:49 +0530

    git

--- git.sh
+++ git.sh
@@ -3,9 +3,10 @@
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]
 then
-    echo -e "\e[1:31m Error:: Permission denied Kinly used with root access \e[0m"
+    echo -e "\e[1;31m Error:: Permission denied Kinly used with root access \e[1;0m"
+        exit 1
 else
-    echo -e "e\1:32m INFO:: Your are in root access \e[0m"
-    exit 1
+    echo -e "e\1;32m INFO:: Your are in root access \e[1;0m"
+    
 fi
 yum install git -y
