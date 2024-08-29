f3ede38b1dc244aeb4eeaa1b00afd00343012303
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-30 01:23:05 +0530

    Delete code

--- delete_log.sh
+++ delete_log.sh
@@ -6,6 +6,11 @@ DELETED_LOG_TO_BE_STORED=/home/ec2-user/shellscript_log
 LOGFILE=$DELETED_LOG_TO_BE_STORED/$SCRIPT_NAME-$DATE.log
 FILE_TO_DELETE=$(find /home/ec2-user/app_log -type f -name "*.log" -mtime 14)
 echo -e "\e[31m $FILE_TO_DELETE \e[0m"
+while read line
+do 
+echo "Deleting $line &>>$LOGFILE"
+rm -rf $line
+done >>> $FILE_TO_DELETE
 
 
 
