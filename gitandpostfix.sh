97813c8aaeb6be468f90558e83333180a5bfed7e
fjjosjsso <sakaraysairam1996@gmail.com>
2024-08-29 16:46:58 +0530

    git

--- gitandpostfix.sh
+++ gitandpostfix.sh
@@ -18,3 +18,21 @@ else
     echo -e "\e[32m INFO::: INSTALLING GIT WAS SUCUSSESFULL \e[0m"
 fi
 
+if [ $USER_ID -ne 0 ]
+then
+    echo -e "\e[31m INFO:: You are not in root access \e[0m"
+    exit 1
+else
+    echo -e "\e[32m INFO::: You are root access \e[0m"
+fi
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+    echo -e "\e[31m ERROR::: INSTALLING POSTFIX WAS FAILD \e[0m"
+else
+    echo -e "\e[32m INFO::: INSTALLING POSTFIX WAS SUCUSSESFULL \e[0m"
+fi
+
+
