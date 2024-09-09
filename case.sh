9750d3f944d3a33ad22c63233228d12c93ce24af
fjjosjsso <sakaraysairam1996@gmail.com>
2024-09-09 19:00:36 +0530

    Mysql download

--- case.sh
+++ case.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo -e "\e[2;34m ::: A FOR To know present working directory \e[3;0m"
-echo -e "\e[2;33m ::: B For to know user id \e[3;0m"
-echo -e "\e[2;35m ::: C For to know date \e[3;0m"
-echo -e "\e[2;36m ::: D For to know scrit name e[3;0m"
+echo -e "\e[1;34m ::: A FOR To know present working directory \e[3;0m"
+echo -e "\e[1;33m ::: B For to know user id \e[3;0m"
+echo -e "\e[1;35m ::: C For to know date \e[3;0m"
+echo -e "\e[1;36m ::: D For to know scrit name \e[3;0m"
 
 read get 
 case $get in
