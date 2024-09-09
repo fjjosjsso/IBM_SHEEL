36baa0fffdc6e025a4df9cf088871abe66abf4e0
fjjosjsso <sakaraysairam1996@gmail.com>
2024-09-09 18:15:22 +0530

    Mysql download

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 usercheck=$(id -u)
 if [ $usercheck -ne 0 ]
+        if [ $@ -eq "mongodb" ]
+        then 
+        sudo vi /etc/yum.repos.d/mongodb-org.repo
+        yum install -y mongodb-org
+        systemctl start mongod
+        systemctl status mongod
+        else
 then
     echo -e "\e[31m Error::: Permission denied your are not in root \e[0m"
     exit 1
 else
     echo -e "\e[32m INFO::: Your are in root access \e[0m"
+    fi
 fi
 
 sudo yum install $@ -y
