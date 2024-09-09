e77bf02b59923b728a9cb10866468d19d5da8b83
fjjosjsso <sakaraysairam1996@gmail.com>
2024-09-09 17:38:50 +0530

    Mysql download

---
+++ mysql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+user-id=(id -u)
+if [ $user-id -ne 0 ]
+then
+    echo "Error::: Permission denied your are not in root"
+    exit 1
+else
+    echo "INFO::: Your are in root access"
+fi
+
+yum install mysql -y
