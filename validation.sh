acd42fe825025a3c94fe11910db6236675da7093
fjjosjsso <sakaraysairam1996@gmail.com>
2024-09-10 00:53:09 +0530

    Function with valiadtion to download any file

--- validation.sh
+++ validation.sh
@@ -4,6 +4,7 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
+P="\e[34m"
 if [ $USER_ID -ne 0 ]
 then
     echo -e "$R ERROR::: YOUR ARE NOT IN ROOT ACCESS KINLDY GET ROOT PERMISSION $N"
@@ -19,5 +20,16 @@ function validation {
         echo -e "$Y............INSTALLING $@ was SUCCESSFULL $N"
     fi
 }
+yum list installed
+function app_check {
+    if [ $3 -ne 0 ]
+    then
+        echo -e "$R ..........INSTALLING $@ WAS NOT INSTALL $N"
+    else 
+        echo -e "$P............INSTALLING $@ was INSTALLED $N"
+    fi
+}
 yum install $@ -y 
-validation $@ 
+validation $?
+aap_check $?
+
