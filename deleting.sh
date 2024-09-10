19f81ab5a310e9c7eafe49b86d8fcf227a9f9ac4
fjjosjsso <sakaraysairam1996@gmail.com>
2024-09-11 03:05:54 +0530

    DELETING LOGS

--- deleting.sh
+++ deleting.sh
@@ -6,4 +6,10 @@ LOG_DIRECTORY=/home/centos/shellscript-logs
 LOGFILE=$LOG_DIRECTORY/$0-$DATE.log
 
 FILE_TO_BE_DELETE=$(find $APP_LOGS_DIRECTORY -name "*.log" -type f -mtime +14)
-echo -e "\e[31m $FILE_TO_BE_DELETE" 
+echo -e "\e[31m $FILE_TO_BE_DELETE \e[0m"
+
+while read line
+do 
+    echo -e "\e[34m $line" & >>$LOGFILE
+    rm -rf $line 
+done <<< $FILE_TO_BE_DELETE
