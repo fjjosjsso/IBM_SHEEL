563b286545fd7d77a39ba810422040a21c572a69
fjjosjsso <sakaraysairam1996@gmail.com>
2024-09-11 00:46:48 +0530

    Delete

--- delete.sh
+++ delete.sh
@@ -2,7 +2,7 @@
 DATE=$(date +%Y//%M//%D)
 LOG_DIRECTORY=/home/centos/shellscript.log
 SCRIPT_NAME=$0
-LOGFILE=$LOG_DIRECTORY/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOG_DIRECTORY/$SCRIPT_NAME-$DATE
 AAP_LOG_DIRECTORY=/home/centos/old.log/
 
 FILE_TO_BE_DELETE=$(find $AAP_LOG_DIRECTORY -type f -name "*.log" -mtime +8)
@@ -11,7 +11,7 @@ echo -e "\e[31m $FILE_TO_BE_DELETE \e[0m"
 #echo -e "\e[33m Exection time and date $DATE \e[0m" &>>$LOGFILE
 while read line
 do
-    echo "Deleting $line"&>>$LOGFILE
+    echo "Deleting $line" &>>$LOGFILE
     rm -rf $line
 done <<< $AAP_LOG_DIRECTORY
 
