3bf0f30ce4e1db7b9fbaab693a0ac10303966435
fjjosjsso <sakaraysairam1996@gmail.com>
2024-09-10 14:51:20 +0530

    Download and log files

---
+++ app_downloads.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#LOGFILES SCRIPT FOR THAT WE NEED "DATE, LOGDIRECTORY=TO WHERE IT CAN BE STORE, SCRIPTNAME"
+DATE=$(date +%Y-%M-%D)
+LOG_DIRECTORY=/home/centos/file.log
+SCRIPT_NAME=$0
+LOGFILES=$LOG_DIRECTORY/$SCRIPT_NAME-$DATE.log
+#COLOURS NOTATIONS
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+#USER CHECK WEATHER USER IS IN ROOT ACCESS OR NOT
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]
+then
+    echo -e "$R ERROR::: YOUR PERMISSION DENIED YOUR ARE NOT IN ROOT ACCESS $N"
+    exit 1
+else
+    echo -e "$G INFO::: YOUR ARE ROOT ACCESS $N"
+fi
+
+function validation {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R ERROR::: INSTALLING $2 FAILED $N"
+    else
+        echo -e "$G INFO::: INSTALLING $2 SUCCESS $N"
+    fi
+}
+for i in $@
+do
+    yum list installed &>>$LOGFILES
+    if [ $? -ne 0 ]
+    then
+        echo -e "$R $@ WAS NOT INSTALLED KINDLY INSTALL $N"
+        yum install $@ -y &>>$LOGFILES
+    else
+        echo -e "$Y $@ WAS INSTALLED ALREDY $N"
+    fi
+done
+validation $? "$@"
